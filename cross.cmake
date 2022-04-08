6aa1a0cb1a6bef2c52380f7b4b8c5b0a7c9f2237
Lars Görner <lars.goerner@plasticlogic.com>
2022-04-08 15:29:32 +0200

    changed compiler flags + forced cmake compiler usage + minor changes

--- cross.cmake
+++ cross.cmake
@@ -1,16 +1,21 @@
+include(CMakeForceCompiler)
+
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(cc_toolchain_path C:/SysGCC/beaglebone)
-set(sysroot_target ${cc_toolchain_path}/arm-linux-gnueabihf/sysroot)
-set(tools ${cc_toolchain_path}/bin)
+set(cc_toolchain_path "C:/SysGCC/beaglebone")
+set(sysroot_target "${cc_toolchain_path}/arm-linux-gnueabihf/sysroot")
+set(tools "${cc_toolchain_path}/bin")
 
-set(CMAKE_C_COMPILER ${tools}/arm-linux-gnueabihf-gcc.exe)
-set(CMAKE_CXX_COMPILER ${tools}/arm-linux-gnueabihf-g++.exe)
-set(CMAKE_SYSROOT ${sysroot_target})
+set(CMAKE_C_COMPILER "${tools}/arm-linux-gnueabihf-gcc.exe")
+set(CMAKE_CXX_COMPILER "${tools}/arm-linux-gnueabihf-g++.exe")
+set(CMAKE_SYSROOT "${sysroot_target}")
+set(CMAKE_FIND_ROOT_PATH "${sysroot_target}")
 
-set(CMAKE_CXX_FLAGS " -march=armv7-a -marm -mfpu=neon -mfloat-abi=hard --sysroot=${sysroot_target}")
-set(CMAKE_C_FLAGS ${CMAKE_CXX_FLAGS})
+set(WARN_FLAGS " -w")
+set(CROSS_FLAGS " -O0 -march=armv7-a -marm -mfpu=neon -mfloat-abi=hard --sysroot=${sysroot_target}")
+set(CMAKE_CXX_FLAGS " -std=gnu++17 ${CROSS_FLAGS} ${WARN_FLAGS}")
+set(CMAKE_C_FLAGS " -std=gnu17 ${CROSS_FLAGS} ${WARN_FLAGS}")
 set(CMAKE_EXE_LINKER_FLAGS "--sysroot=${sysroot_target}")
 
 set(CMAKE_FIND_ROOT_MODE_PROGRAM NEVER)
