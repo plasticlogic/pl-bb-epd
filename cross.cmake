6786ba50f194e16186d515b3c4b64fccd1407211
Lars Görner <lars.goerner@plasticlogic.com>
2022-04-19 15:09:25 +0200

    Made compiler warnings and optimizations optionable

--- cross.cmake
+++ cross.cmake
@@ -12,10 +12,8 @@ set(CMAKE_CXX_COMPILER "${tools}/arm-linux-gnueabihf-g++.exe")
 set(CMAKE_SYSROOT "${sysroot_target}")
 set(CMAKE_FIND_ROOT_PATH "${sysroot_target}")
 
-set(WARN_FLAGS " -w")
-set(CROSS_FLAGS " -O0 -march=armv7-a -marm -mfpu=neon -mfloat-abi=hard --sysroot=${sysroot_target}")
-set(CMAKE_CXX_FLAGS " -std=gnu++17 ${CROSS_FLAGS} ${WARN_FLAGS}")
-set(CMAKE_C_FLAGS " -std=gnu17 ${CROSS_FLAGS} ${WARN_FLAGS}")
+set(CROSS_FLAGS " -march=armv7-a -marm -mfpu=neon -mfloat-abi=hard --sysroot=${sysroot_target}")
+set(CMAKE_C_FLAGS " -std=gnu17 ${CROSS_FLAGS}")
 set(CMAKE_EXE_LINKER_FLAGS "--sysroot=${sysroot_target}")
 
 set(CMAKE_FIND_ROOT_MODE_PROGRAM NEVER)
